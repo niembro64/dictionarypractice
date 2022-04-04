577dd5cde65cf7ab516811bdb8f694c6da100229
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-04-04 13:48:02 -0400

    done

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dictionarypractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> profile = new Dictionary<string, string>();
+            //Almost all the methods that exists with Lists are the same with Dictionaries
+            profile.Add("Name", "Speros");
+            profile.Add("Language", "PHP");
+            profile.Add("Location", "Greece");
+            Console.WriteLine("Instructor Profile");
+            Console.WriteLine("Name - " + profile["Name"]);
+            Console.WriteLine("From - " + profile["Location"]);
+            Console.WriteLine("Favorite Language - " + profile["Language"]);
+
+            //////////////////
+            Console.WriteLine();
+
+            foreach (KeyValuePair<string, string> entry in profile)
+            {
+                Console.WriteLine(entry.Key + " - " + entry.Value);
+            }
+
+            ////////////////////////
+            Console.WriteLine();
+
+            //The var keyword takes the place of a type in type-inference
+            foreach (var entry in profile)
+            {
+                Console.WriteLine(entry.Key + " - " + entry.Value);
+            }
+
+
+
+
+
+
+        }
+    }
+}
